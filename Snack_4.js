c41a60df0c8a241c3cbfbd7681c0fbe8bcea70db
MyUsername <my@email.com>
2022-11-28 22:46:33 +0100

    Create all let

--- Snack_4.js
+++ Snack_4.js
@@ -1,1 +1,7 @@
 // Prendere due array della stessa lunghezza e generarne un terzo, prendendo alternativamente gli elementi da uno e dall'altro es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]
+
+let listNums = [1,2,3,4,5,6,7,8,9]
+
+let letterList = ["a","b","c","d","e","f","g","h","i"]
+
+let allList = []
