113007c113d04cd9face92469a9e43836ea97236
MyUsername <my@email.com>
2022-11-28 22:26:12 +0100

    Create numsList

---
+++ Snack_3.js
@@ -0,0 +1,6 @@
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+const numsListOne = [4,2,45,5,700,345,7,59,734,35,43,36,54,77,6,3456,31,23]
+
+
+const numsListTwo = [34,345,2889,765,6776,53,345,,123,65]
