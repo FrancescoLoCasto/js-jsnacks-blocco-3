debe0ef77d23d9ccfd440d95ecd4666e0381d241
MyUsername <my@email.com>
2022-11-28 22:28:58 +0100

    Create while cicle

--- Snack_3.js
+++ Snack_3.js
@@ -4,3 +4,8 @@ const numsListOne = [4,2,45,5,700,345,7,59,734,35,43,36,54,77,6,3456,31,23]
 
 
 const numsListTwo = [34,345,2889,765,6776,53,345,,123,65]
+
+while (numsListOne.length !== numsListTwo.length) {
+
+   
+}
