50d2d3c2c40bfd457b4985dfb85f31b59c6e6d57
MyUsername <my@email.com>
2022-11-28 21:52:41 +0100

    create of let

--- Snack_1.js
+++ Snack_1.js
@@ -1,1 +1,8 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+   let numbersOneOfTen= parseInt(prompt("Prova ad inserire un numero che va da 1 a 10"));
+
+   while (numbersOneOfTen< 1 || numbersOneOfTen> 10){
+   numbersOneOfTen= parseInt(prompt("Prova ad inserire un numero che va da 1 a 10"));
+   console.log(userNumber);
+   }
